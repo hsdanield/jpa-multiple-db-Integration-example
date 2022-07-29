5ac6d4e38c62f8071da474bafcd0042baaced88f
hsdanield <hsdanield@gmail.com>
2022-07-29 16:59:56 -0300

    Ajustando Base de dados MySQL e PostgreSQL

--- src/main/java/com/jumperdb/config/datasource/MysqlDataSourceConfig.java
+++ src/main/java/com/jumperdb/config/datasource/MysqlDataSourceConfig.java
@@ -1,5 +1,6 @@
 package com.jumperdb.config.datasource;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
@@ -7,12 +8,11 @@ import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
@@ -24,19 +24,37 @@ import javax.sql.DataSource;
         transactionManagerRef= "mysqlTransactionManager"
 )
 public class MysqlDataSourceConfig {
+    @Autowired
+    private Environment environment;
 
     @Primary
     @Bean(name = "mysqlDataSource")
     @ConfigurationProperties(prefix = "spring.datasource-mysql")
     public DataSource mysqlDataSource() {
-        return DataSourceBuilder.create().build();
+        return DataSourceBuilder.create().driverClassName(environment.getProperty("spring.datasource-mysql.driverClassName")).build();
+
+//        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+//        dataSource.setUrl(env.getProperty("spring.datasource-mysql.jdbcUrl"));
+//        dataSource.setUsername(env.getProperty("spring.datasource-mysql.username"));
+//        dataSource.setPassword(env.getProperty("spring.datasource-mysql.password"));
+//        dataSource.setDriverClassName(env.getProperty("spring.datasource-mysql.driverClassName"));
+//        return dataSource;
+
     }
 
     @Primary
     @Bean(name = "mysqlEntityManager")
     public LocalContainerEntityManagerFactoryBean mysqlEntityManager(EntityManagerFactoryBuilder builder, @Qualifier("mysqlDataSource") DataSource dataSource) {
+
+        /*
+        *final HashMap<String, Object> properties = new HashMap<String, Object>();
+        *properties.put("hibernate.hbm2ddl.auto", environment.getProperty("spring.datasource-postgresql.custom-param.hibernate.ddl-auto"));
+        */
+
+
         return builder
                 .dataSource(dataSource)
+//                .properties()
                 .packages("com.jumperdb.domain.mysql.model")
                 .persistenceUnit("mysql-persist")
                 .build();
--- src/main/java/com/jumperdb/config/datasource/PostgresqlDataSourceConfig.java
+++ src/main/java/com/jumperdb/config/datasource/PostgresqlDataSourceConfig.java
@@ -1,12 +1,13 @@
 package com.jumperdb.config.datasource;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
+import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
@@ -23,19 +24,29 @@ import javax.sql.DataSource;
 )
 public class PostgresqlDataSourceConfig {
 
+    @Autowired
+    private Environment environment;
+
     @Bean(name = "postgresqlDataSource")
     @ConfigurationProperties(prefix = "spring.datasource-postgresql")
     public DataSource postgresqlDataSource() {
-        return DataSourceBuilder.create().build();
+        return DataSourceBuilder.create().driverClassName(environment.getProperty("spring.datasource-postgresql.driverClassName")).build();
     }
 
     @Bean(name = "postgresqlEntityManager")
     public LocalContainerEntityManagerFactoryBean postgresqlEntityManager(EntityManagerFactoryBuilder builder, @Qualifier("postgresqlDataSource") DataSource dataSource) {
+         /*
+         *final HashMap<String, Object> properties = new HashMap<String, Object>();
+         *properties.put("hibernate.hbm2ddl.auto", environment.getProperty("spring.datasource-postgresql.custom-param.hibernate.ddl-auto"));
+         */
+
         return builder
                 .dataSource(dataSource)
+//                .properties()
                 .packages("com.jumperdb.domain.postgresql.model")
                 .persistenceUnit("postgresql-persist")
                 .build();
+
     }
 
     @Bean(name = "postgresqlTransactionManager")
